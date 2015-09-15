8aa94ebec32fb385c791d2686b17d0f3bf42f7b5
liu-rui <1313475@qq.com>
2015-09-15 18:04:24 +0800

    add

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
-package  main
+package main
 
 import (
+	"net/http"
 	"fmt"
 )
 
-
-func  main(){
-	fmt.Println("hello world")
+func main() {
+	http.HandleFunc("/", func(rep http.ResponseWriter, req *http.Request) {
+		rep.Write([]byte("hello"))
+	})
+	fmt.Println("listening  5642")
+	http.ListenAndServe(":5642", nil)	
 }
